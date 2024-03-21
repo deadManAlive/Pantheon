10e2025f04b1f9292815329926c27f684c4550d5
deadManAlive <alfarrakhan@gmail.com>
2024-03-21 15:59:41 +0700

    basic implementation of APF (thread safe?)

--- Processors.h
+++ Processors.h
@@ -181,13 +181,17 @@ namespace process {
 
             void prepareToPlay(double sampleRate, int samplesPerBlock) override {
                 maxDelayInSamples = samplesPerBlock / 2;
+                _sampleRate = sampleRate / 2;
 
                 delayParamSmoothedValue.reset(samplesPerBlock / 4);
+                filterParamSmoothedValue.reset(samplesPerBlock / 4);
 
                 fxUnitProcessor->get<0>().setMaximumDelayInSamples(maxDelayInSamples / 2);
+                fxUnitProcessor->get<1>().coefficients = Coefficients::makeAllPass(sampleRate, 10);
                 fxUnitProcessor->prepare(
                     {sampleRate, (uint32)samplesPerBlock, 1}
                 );
+                fxUnitProcessor->reset();
             }
 
             void processBlock(AudioSampleBuffer& buffer, MidiBuffer&) override {
@@ -207,31 +211,46 @@ namespace process {
 
             //==============================================================================
             int maxDelayInSamples { 128 };
+            double _sampleRate { 44100. };
 
             //==============================================================================
-            using FxProcess = dsp::ProcessorChain<dsp::DelayLine<float>>;
+            using FxProcess = dsp::ProcessorChain<dsp::DelayLine<float>, dsp::IIR::Filter<float>>;
+            using Coefficients = dsp::IIR::Coefficients<float>;
 
             std::unique_ptr<FxProcess> fxUnitProcessor;
 
             //==============================================================================
             LinearSmoothedValue<float> delayParamSmoothedValue;
+            LinearSmoothedValue<float> filterParamSmoothedValue;
 
             //==============================================================================
+            // TODO: logarithmize all pass filter slider + why the shit is it inverted in FL?
             void updateParameter() {
                 const auto delayParam = parameters.getRawParameterValue("delayLine");
+                const auto filterParam = parameters.getRawParameterValue("allPassFreq");
+
                 const auto delayParamValue = delayParam->load();
+                const auto filterParamValue = filterParam->load();
 
                 delayParamSmoothedValue.setTargetValue(delayParamValue);
+                filterParamSmoothedValue.setTargetValue(filterParamValue);
 
                 const auto currentDelayValue = delayParamSmoothedValue.getNextValue();
+                const auto currentFilterValue = filterParamSmoothedValue.getNextValue();
+
+                float delay;
+                float filter;
 
                 if (CHANNEL == Left) {
-                    const auto delay = abs(jlimit(-1.f, 0.f, currentDelayValue)) * static_cast<float>(maxDelayInSamples);
-                    fxUnitProcessor->get<0>().setDelay(delay);
+                    delay = abs(jlimit(-1.f, 0.f, currentDelayValue)) * static_cast<float>(maxDelayInSamples);
+                    filter = abs(jlimit(-1.f, 0.f, currentFilterValue)) * static_cast<float>(_sampleRate / 2);
                 } else {
-                    const auto delay = jlimit(0.f, 1.f, currentDelayValue) * static_cast<float>(maxDelayInSamples);
-                    fxUnitProcessor->get<0>().setDelay(delay);
+                    delay = jlimit(0.f, 1.f, currentDelayValue) * static_cast<float>(maxDelayInSamples);
+                    filter = jlimit(0.f, 1.f, currentDelayValue) * static_cast<float>(_sampleRate / 2);
                 }
+
+                fxUnitProcessor->get<0>().setDelay(delay);
+                fxUnitProcessor->get<1>().coefficients = Coefficients::makeAllPass(_sampleRate, filter > 10.f ? filter : 10.f);
             }
 
             //==============================================================================
